5c37f538a38d0165875749f11d2888d8c3864a31
Dada97400 <humblot.david1@gmail.com>
2020-11-09 15:25:32 +0400

    test

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-var request = new XMLHttpRequest();
-request.open("POST", "http://url-service-web.com/api/users");
+
+
+
+function send (){
+
+    var request = new XMLHttpRequest();
+request.open("POST", "https://mockbin.com/request");
 request.setRequestHeader("Content-Type", "application/json");
-request.send(JSON.stringify(jsonBody));
+request.send(JSON.stringify(value));
+
+value=document.getElementById("value").value;
+resultat=document.getElementById("result");
+resultat.innerHTML=value;
+
+request.onreadystatechange = function() {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        var response = JSON.parse(this.postData.text);
+        console.log(this.responseText);
+    }
+
+};
+}
+
+
+
+
+
+
+
+
+
