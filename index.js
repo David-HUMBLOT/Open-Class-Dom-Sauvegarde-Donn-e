b126fd6db8efd3e315a63da013c293fcc1e80c95
Dada97400 <humblot.david1@gmail.com>
2020-11-10 09:01:40 +0400

    function

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 
-
-
-const saisietxt = document.getElementById("result");
-const value = document.getElementById("value").value;  /**récupère les donnée saisie par l'utilisateur */
-
-/**écouteur d'événement sur la saisie texte  */
-
-ecoutesaisie = addEventListener ('input', updatesaisie);
-function updatesaisie(e){
-    
-}
+    const saisietxt = document.getElementById("result");
+    /**récupère les donnée saisie par l'utilisateur */
+    const value = document.getElementById("value").value;  
+    /**écouteur d'événement sur la saisie texte  */
+    ecoutesaisie = addEventListener ('input', updatesaisie);
+    function updatesaisie(e){
+        saisietxt.textContent = e.target.value;
+    }
 
 function send (){
-
-    var resultat = document.getElementById("result");
+    /**requete d'envoi JSON modifier en text */
+    /**inscription des donnée récupérer par value et par écouteur d'événement */
     var request = new XMLHttpRequest();
-    
-request.open("POST", "https://mockbin.com/request");
-request.setRequestHeader("Content-Type", "application/json");
-request.send(JSON.stringify(value)); /**requete d'envoi JSON modifier en text */
+    request.open("POST", "https://mockbin.com/request");
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify(value)); 
 
-resultat.innerHTML=value; /**inscription des donnée récupéré par value et par écouteur d'événement */
+    saisietxt.innerHTML=value;
 
+    /**requète de récupération de JSON  */
 
-request.onreadystatechange = function() {
+    request.onreadystatechange = function() {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         var response = JSON.parse(this.postData.text);
         console.log(this.responseText);
