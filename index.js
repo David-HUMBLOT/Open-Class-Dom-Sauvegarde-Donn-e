8f9637d579bc450a060eb88d483c1c5c045ae04b
Dada97400 <humblot.david1@gmail.com>
2020-11-10 10:24:09 +0400

    exofini

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 
-    const saisietxt = document.getElementById("result");
-    /**récupère les donnée saisie par l'utilisateur */
-     
-    /**écouteur d'événement sur la saisie texte  */
-    ecoutesaisie = addEventListener ('input', updatesaisie);
-    function updatesaisie(e){
-        saisietxt.textContent = e.target.value;
-    }
 
-function send (){
 
-    /**requete d'envoi JSON modifier en text */
-    /**inscription des donnée récupérer par value et par écouteur d'événement */
+
+const saisietxt = document.getElementById("result");
+/**récupère les donnée saisie par l'utilisateur */
+
+/**écouteur d'événement sur la saisie texte et requete d'envoi et de réception  */
+function send() {
+
     var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            saisietxt.innerHTML = response.postData.text;
+        }
+
+        /**requete d'envoi JSON modifier en text */
+        /**inscription des donnée récupérer par value et par écouteur d'événement */  
+    };
+    
+/**requète de récupération de JSON apres modif saisie txt user  */
+
     request.open("POST", "https://mockbin.com/request");
     request.setRequestHeader("Content-Type", "application/json");
-    request.send(JSON.stringify({value: document.getElementById("value").value})); 
+    request.send(JSON.stringify({ value: document.getElementById("value").value }));
 
-    saisietxt.innerHTML=value;
-
-    /**requète de récupération de JSON apres modif saisie txt user  */
-
-    request.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.postData.text);
-        console.log(this.responseText);
-    }
-};
 }
+
+var ecouteform = document.getElementById("form");
+ecouteform.addEventListener('submit', function (event) {
+    send();
+    event.preventDefault();
+});
+
+
 
 
 
