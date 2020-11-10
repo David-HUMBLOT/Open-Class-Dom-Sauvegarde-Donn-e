3f1dda132409c4dd86653d3667206a5db156d22c
Dada97400 <humblot.david1@gmail.com>
2020-11-10 08:36:39 +0400

    function

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 
 
 
+const saisietxt = document.getElementById("result");
+const value = document.getElementById("value").value;  /**récupère les donnée saisie par l'utilisateur */
+
+/**écouteur d'événement sur la saisie texte  */
+
+ecoutesaisie = addEventListener ('input', updatesaisie);
+function updatesaisie(e){
+    
+}
+
 function send (){
 
+    var resultat = document.getElementById("result");
     var request = new XMLHttpRequest();
+    
 request.open("POST", "https://mockbin.com/request");
 request.setRequestHeader("Content-Type", "application/json");
-request.send(JSON.stringify(value));
+request.send(JSON.stringify(value)); /**requete d'envoi JSON modifier en text */
 
-value=document.getElementById("value").value;
-resultat=document.getElementById("result");
-resultat.innerHTML=value;
+resultat.innerHTML=value; /**inscription des donnée récupéré par value et par écouteur d'événement */
+
 
 request.onreadystatechange = function() {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
